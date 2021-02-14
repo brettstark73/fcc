d4f73610de4f8eb01b2b7032baa3c0f6afab9244
Brett Stark <brett@brettstark.com>
2021-02-14 12:00:22 -0600

    Update shape_calculator.py

--- polygon/shape_calculator.py
+++ polygon/shape_calculator.py
@@ -1,6 +1,58 @@
 class Rectangle:
 
+# help / reference to https://repl.it/@SpencerSahu/fcc-shape-calculator - thanks.
+# @SpencerSahu
+
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+  
+  def __str__(self):
+    #return(("Rectangle(width={0}, height={1})").format(self.width, self.height))
+    return("Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")")
+
+  def set_width(self, value):
+    self.width = value
+
+  def set_height(self, value):
+    self.height = value
+
+  def get_area(self):
+    return(self.width * self.height)
+
+  def get_perimeter(self):
+    return(2 * self.width + 2 * self.height)
+
+  def get_diagonal(self):
+    return((self.width ** 2 + self.height ** 2) ** .5)
+
+  def get_picture(self):
+    if (self.width > 50 or self.height > 50): 
+      return("Too big for picture.")
+    else:
+      return((("*" * self.width)+ "\n" ) * self.height)
+  
+  def get_amount_inside(self, Shape):
+    return(self.get_area() // Shape.get_area())
 
 
+class Square(Rectangle):
+  def __init__(self, side):
+    Rectangle.width = side
+    Rectangle.height = side
 
-class Square:
+  def __str__(self):
+    return("Square(side=" + str(self.width) + ")")
+
+  def set_side(self, side):
+    Rectangle.width = side
+    Rectangle.height = side
+
+  def set_width(self, side):
+    Rectangle.width = side
+    Rectangle.height = side
+
+  def set_height(self, side):
+    Rectangle.width = side
+    Rectangle.height = side
+    
